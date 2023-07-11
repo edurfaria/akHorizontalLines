17da9e59a212891e558a13b856ab48f090e51d42
Eduardo Faria <efaria@hes-inovacao.com>
2023-07-11 16:53:10 +0100

    Initial Setting for VisualBox

--- app/src/main/java/com/example/akhorizontallines/Canvas.kt
+++ app/src/main/java/com/example/akhorizontallines/Canvas.kt
@@ -7,10 +7,19 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import java.util.Objects
 import kotlin.math.max
 import kotlin.math.min
 
 class Canvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    data class RectangleData(
+        val isRectangleVisible: Boolean,
+        val rectangleLeft: Float,
+        val rectangleTop: Float,
+        val rectangleRight: Float,
+        val rectangleBottom: Float
+    )
+
     // Coordinates for the dot (Circle)
     private var x = 0.0F
     private var y = 0.0F
@@ -33,54 +42,86 @@ class Canvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
             style = Paint.Style.FILL
         }
 
-        val rectanglePaint = Paint().apply {
-            color = Color.RED
+        //VisualBox Paints
+        val boxFillPaint = Paint().apply {
+            style = Paint.Style.FILL
+            color = Color.argb(128, 255, 127, 80)
+        }
+
+        val boxStrokePaint = Paint().apply {
             style = Paint.Style.STROKE
-            strokeWidth = 5F
+            strokeWidth = 15f
+            color = Color.argb(255, 255, 127, 80)
         }
 
         if (isRectangleVisible) {
-            Log.d("Debug", "//Re-Drawing...")
+            //Log.d("Debug", "//Re-Drawing...")
 
             canvas?.drawCircle(x, y, radius, paint)
-
+            /**
+             * Drawing two rectangles to give the illusion of box with border
+             */
             canvas?.drawRect(
                 rectangleLeft,
                 rectangleTop,
                 rectangleRight,
                 rectangleBottom,
-                rectanglePaint
+                boxFillPaint
+            )
+            canvas?.drawRect(
+                rectangleLeft,
+                rectangleTop,
+                rectangleRight,
+                rectangleBottom,
+                boxStrokePaint
             )
         }
     }
 
+    /**
+     * Update Position of dot
+     * @deprecated
+     */
     fun updatePosition(x: Float, y: Float) {
         this.x = x
         this.y = y
         invalidate() // Invalidate the view to trigger a redraw
     }
 
-    fun drawRectangle(x: Float, y: Float, scale: Int = 2) {
-        val containerWidth = width.toFloat()
-        val containerHeight = height.toFloat()
-        Log.d("Box", "width:$containerWidth - height:$containerHeight")
+//    fun drawRectangle(x: Float, y: Float, scale: Int = 2) {
+//        //Screen Info
+//        val containerWidth = width.toFloat()
+//        val containerHeight = height.toFloat()
+//
+//        //Center coordinates
+//        val centerX = x - ((containerWidth / scale) / 2)
+//        val centerY = y - ((containerHeight / scale) / 2)
+//
+//        //Possible Max
+//        val maxX = containerWidth - (containerWidth / scale)
+//        val maxY = containerHeight - (containerHeight / scale)
+//
+//        //Bounded Values
+//        val boundedX = min(max(centerX, 0f), maxX)
+//        val boundedY = min(max(centerY, 0f), maxY)
+//
+//        val left = boundedX
+//        val top = boundedY
+//        val right = boundedX + (containerWidth / scale)
+//        val bottom = boundedY + (containerHeight / scale)
+//
+//        this.x = x
+//        this.y = y
+//        this.rectangleLeft = left
+//        this.rectangleTop = top
+//        this.rectangleRight = right
+//        this.rectangleBottom = bottom
+//        this.isRectangleVisible = true
+//
+//        invalidate() // Invalidate the view to trigger a redraw
+//    }
 
-        val centerX = x - ((containerWidth / scale) / 2)
-        val centerY = y - ((containerHeight / scale) / 2)
-
-        val maxX = containerWidth - (containerWidth / scale)
-        val maxY = containerHeight - (containerHeight / scale)
-
-        val boundedX = min(max(centerX, 0f), maxX)
-        val boundedY = min(max(centerY, 0f), maxY)
-
-        val left = boundedX
-        val top = boundedY
-        val right = boundedX + (containerWidth / scale)
-        val bottom = boundedY + (containerHeight / scale)
-
-        this.x = x
-        this.y = y
+    fun drawVisualBox(left: Float, top: Float, right: Float, bottom: Float) {
         this.rectangleLeft = left
         this.rectangleTop = top
         this.rectangleRight = right
@@ -95,4 +136,17 @@ class Canvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
     }
 
 
+    fun firstStage() {
+
+    }
+
+    fun isRectangleVisible(): RectangleData {
+        return RectangleData(
+            isRectangleVisible,
+            rectangleLeft,
+            rectangleTop,
+            rectangleRight,
+            rectangleBottom
+        )
+    }
 }
--- app/src/main/java/com/example/akhorizontallines/MainActivity.kt
+++ app/src/main/java/com/example/akhorizontallines/MainActivity.kt
@@ -1,15 +1,49 @@
 package com.example.akhorizontallines
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.TimeInterpolator
+import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.LinearInterpolator
 import android.widget.Button
 import com.example.akhorizontallines.databinding.ActivityMainBinding
+import java.lang.Float.max
+import java.lang.Float.min
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var duration: Long = 1000
+
+    private var workingY = false
+    private var workingX = false
+    private var translateY: Float = 0.0F
+    private var translateX: Float = 0.0F
+
+    private lateinit var animateX: ValueAnimator
+    private lateinit var animateY: ValueAnimator
+
+    data class RectangleData(
+        val isRectangleVisible: Boolean,
+        val rectangleLeft: Float,
+        val rectangleTop: Float,
+        val rectangleRight: Float,
+        val rectangleBottom: Float
+    )
+
+    //Vars for Rectangle
+    private var isRectangleVisible = false
+
+    private var rectangleLeft = 0F
+    private var rectangleTop = 0F
+    private var rectangleRight = 0F
+    private var rectangleBottom = 0F
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +60,15 @@ class MainActivity : AppCompatActivity() {
      */
     @SuppressLint("ClickableViewAccessibility")
     private fun setupHorizontalLines() {
+        binding.sHorizontal.hide()
+        binding.sVertical.hide()
         binding.screen.setOnTouchListener { _, event ->
             when (event.actionMasked) {
                 MotionEvent.ACTION_DOWN -> {
                     val x = event.x
                     val y = event.y
                     // do something with x and y coordinates
-                    binding.canvas.drawRectangle(x, y)
+                    touchableEvent(x, y)
                     true
                 }
 
@@ -41,6 +77,132 @@ class MainActivity : AppCompatActivity() {
         }
 
         setButtons(false)
+    }
+
+    private fun touchableEvent(x: Float, y: Float) {
+        Log.d(
+            "Box",
+            "//Click Detected: x:$x y:$y"
+        )
+
+
+
+        when {
+            (workingX && !workingY) -> {
+                workingX = false
+                workingY = true
+                translateX = binding.sVertical.translationX
+                binding.sVertical.translationX = translateX
+                binding.sHorizontal.show()
+                binding.sText.text = "2º Stage"
+                secondStage(rectangleTop, rectangleBottom)
+            }
+
+            (!workingX && !workingY) -> {
+                val process = processVisualBox(x, y)
+                Log.d("Box", "Process: $process")
+                //Working X Axis
+                workingX = true
+                workingY = false
+                //translateY = 0.0F
+                //translateX = process.rectangleLeft
+                //binding.sVertical.translationX = process.rectangleLeft
+                binding.sVertical.show()
+                binding.canvas.drawVisualBox(
+                    process.rectangleLeft,
+                    process.rectangleTop,
+                    process.rectangleRight,
+                    process.rectangleBottom
+                )
+                binding.sText.text = "1º Stage"
+                firstStage(process.rectangleLeft, process.rectangleRight)
+            }
+        }
+    }
+
+    /**
+     * secondStage
+     * @description Call animation on sHorizontal View
+     * @param from starting Y
+     * @param to ending Y
+     *
+     * @see touchableEvent
+     */
+    private fun secondStage(from: Float, to: Float) {
+        Log.d("Debug", "//Starting 2º Stage...")
+
+        this.animateY = animateViewFromTopToBottom(
+            binding.sHorizontal,
+            duration,
+            from,
+            to,
+            LinearInterpolator()
+        ) {
+            Log.d("Animation", "Animation Y ended")
+            workingY = false
+        }
+        this.animateY.start()
+    }
+
+    /**
+     * firstStage
+     * @description Call animation on sVertical View
+     * @param from starting X
+     * @param to ending X
+     *
+     * @see touchableEvent
+     */
+    private fun firstStage(from: Float, to: Float) {
+        Log.d("Debug", "//Starting 1º Stage...")
+        this.animateX=animateViewFromLeftToRight(
+            binding.sVertical,
+            duration,
+            from,
+            to,
+            LinearInterpolator()
+        ) {
+            Log.d("Animation", "Animation X ended")
+            workingX = false
+        }
+        this.animateX.start()
+    }
+
+    private fun processVisualBox(x: Float, y: Float, scale: Int = 2): RectangleData {
+        //Screen Info
+        val containerWidth = resources.displayMetrics.widthPixels.toFloat()
+        val containerHeight = resources.displayMetrics.heightPixels.toFloat()
+
+        //Center coordinates
+        val centerX = x - ((containerWidth / scale) / 2)
+        val centerY = y - ((containerHeight / scale) / 2)
+
+        //Possible Max
+        val maxX = containerWidth - (containerWidth / scale)
+        val maxY = containerHeight - (containerHeight / scale)
+
+        //Bounded Values
+        val boundedX = min(max(centerX, 0f), maxX)
+        val boundedY = min(max(centerY, 0f), maxY)
+
+        val left = boundedX
+        val top = boundedY
+        val right = boundedX + (containerWidth / scale)
+        val bottom = boundedY + (containerHeight / scale)
+
+        //Later debug if needed
+        this.rectangleLeft = left
+        this.rectangleTop = top
+        this.rectangleRight = right
+        this.rectangleBottom = bottom
+        this.isRectangleVisible = true
+
+        return RectangleData(
+            isRectangleVisible,
+            rectangleLeft,
+            rectangleTop,
+            rectangleRight,
+            rectangleBottom
+        )
     }
 
     private fun setButtons(clickable: Boolean = false) {
@@ -61,5 +223,92 @@ class MainActivity : AppCompatActivity() {
     private fun setMessage(input: String) {
         Log.d("Debug", "//Input Received $input")
         binding.sResult.text = input
+    }
+
+    fun View.hide() {
+        this.visibility = View.GONE
+    }
+
+    fun View.show() {
+        this.visibility = View.VISIBLE
+    }
+
+    /**
+     * animateViewFromLeftToRight
+     * @description Generic Animation from the left to the right, on a X axis
+     * @param view ID of a View to Manipulate
+     * @param duration Duration of the animation in ms
+     * @param from Starting Value for animation (Float)
+     * @param to Ending Value for animation (Float)
+     * @param onAnimationEnd Callback once Animation ends
+     * @author Eduardo Faria
+     *
+     * @see firstStage
+     */
+    private fun animateViewFromLeftToRight(
+        view: View,
+        duration: Long,
+        from: Float = 0.0F,
+        to: Float = 0.0F,
+        easing: TimeInterpolator = AccelerateDecelerateInterpolator(),
+        onAnimationEnd: () -> Unit
+    ): ValueAnimator {
+        //If i provide a to, i dont need to calculate default
+        val start = if (from != 0.0F) from else 0.0F
+        val end = if (to != 0.0F) to else resources.displayMetrics.heightPixels.toFloat()
+        val animator = ValueAnimator.ofFloat(start, end)
+        animator.duration = duration
+        animator.interpolator = easing
+        animator.addUpdateListener {
+            val value = it.animatedValue as Float
+            view.translationX = value
+
+        }
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                onAnimationEnd()
+            }
+        })
+        return animator
+    }
+
+    /**
+     * animateViewFromTopToBottom
+     * @description Generic Animation from the top to the bottom, on a Y axis
+     * @param view ID of a View to Manipulate
+     * @param duration Duration of the animation in ms
+     * @param from Starting Value for animation (Float)
+     * @param to Ending Value for animation (Float)
+     * @param onAnimationEnd Callback once Animation ends
+     *
+     * @author Eduardo Faria
+     * @see secondStage
+     */
+    private fun animateViewFromTopToBottom(
+        view: View,
+        duration: Long,
+        from: Float = 0.0F,
+        to: Float = 0.0F,
+        easing: TimeInterpolator = AccelerateDecelerateInterpolator(),
+        onAnimationEnd: () -> Unit
+    ): ValueAnimator {
+        //If i provide a to, i dont need to calculate default
+        val start = if (from != 0.0F) from else 0.0F
+        val end = if (to != 0.0F) to else resources.displayMetrics.heightPixels.toFloat()
+        val animator = ValueAnimator.ofFloat(start, end)
+        animator.duration = duration
+        animator.interpolator = easing
+        animator.addUpdateListener {
+            val value = it.animatedValue as Float
+            view.translationY = value
+
+        }
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                onAnimationEnd()
+            }
+        })
+        return animator
+        //animator.start()
     }
 }
